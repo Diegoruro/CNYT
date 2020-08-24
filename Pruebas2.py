c35a4fa9ec8ef2f4c7b99869cc4cc37c3c1c8044
Diegoruro <diegofernandoruizrojas@gmail.com>
2020-08-23 23:28:32 -0500

    avance y modificacion nueva

---
+++ Pruebas2.py
@@ -0,0 +1,365 @@
+import unittest
+import new_complejos
+from new_complejos import *
+
+
+class Testcomplex(unittest.TestCase):
+    def test_suma(self):
+        # primer_test
+        resultado = new_complejos.suma([1, 2], [3, 4])
+        self.assertEqual(resultado, (4, 6))
+        # segundo_test
+        resultado = new_complejos.suma([10, 3], [2, 19])
+        self.assertEqual(resultado, (12, 22))
+        # tercer_test
+        resultado = new_complejos.suma([2.5, 3], [1.5, 3.5])
+        self.assertEqual(resultado, (4.0, 6.5))
+        # cuarto_test
+        resultado = new_complejos.suma([2 * 2, 4 * 3], [1 * 10, 3 * 3])
+        self.assertEqual(resultado, (14, 21))
+
+    def test_resta(self):
+        # primer_test
+        resultado = new_complejos.resta([3, 9], [1, 5])
+        self.assertEqual(resultado, (2, 4))
+        # segundo_test
+        resultado = new_complejos.resta([2, 9], [2, 9])
+        self.assertEqual(resultado, (0, 0))
+        # tercer_test
+        resultado = new_complejos.resta([2.5, 0], [4, 1])
+        self.assertEqual(resultado, (-1.5, -1))
+        # cuarto_test
+        resultado = new_complejos.resta([2 / 2, 2], [1, 12 / 3])
+        self.assertEqual(resultado, (0, -2))
+
+    def test_multi(self):
+        # primer_test
+        resultado = new_complejos.multi([3, 9], [1, 5])
+        self.assertEqual(resultado, (-42, 24))
+        # segundo_test
+        resultado = new_complejos.multi([1, 6], [6, 1])
+        self.assertEqual(resultado, (0, 37))
+        # tercer_test
+        resultado = new_complejos.multi([0, 20], [4, 3])
+        self.assertEqual(resultado, (-60, 80))
+        # cuarto_test
+        resultado = new_complejos.multi([4, 1.5], [0.5, 3])
+        self.assertEqual(resultado, (-2.5, 12.75))
+
+    def test_divi(self):
+        # primer_test
+        resultado = new_complejos.division([6, 5], [3, 2])
+        self.assertEqual(resultado, (2.154, 0.231))
+        # segundo_test
+        resultado = new_complejos.division([6, 5], [5, 6])
+        self.assertEqual(resultado, (0.984, -0.180))
+        # tercer_test
+        resultado = new_complejos.division([5, 20], [10, 2])
+        self.assertEqual(resultado, (0.865, 1.827))
+        # cuarto_test
+        resultado = new_complejos.division([20 / 10, 50 / 5], [10 / 5, 0.5])
+        self.assertEqual(resultado, (2.118, 4.471))
+
+    def test_modulo(self):
+        # primer_test
+        resultado = new_complejos.modulo([1, 1])
+        self.assertEqual(resultado, 1.41)
+        # segundo_test
+        resultado = new_complejos.modulo([3, 4])
+        self.assertEqual(resultado, 5)
+        # tercer_test
+        resultado = new_complejos.modulo([1 + 2, 4 + 3])
+        self.assertEqual(resultado, 7.62)
+        # cuarto_test
+        resultado = new_complejos.modulo([0.5, 1.5])
+        self.assertEqual(resultado, 1.58)
+
+    def test_conjugado(self):
+        # primer_test
+        resultado = new_complejos.conjugado([1, 1])
+        self.assertEqual(resultado, (1, -1))
+        # segundo_test
+        resultado = new_complejos.conjugado([1, -6])
+        self.assertEqual(resultado, (1, 6))
+        # tercer_test
+        resultado = new_complejos.conjugado([1, -1.5])
+        self.assertEqual(resultado, (1, 1.5))
+        # cuarto_test
+        resultado = new_complejos.conjugado([1, 20 / 2])
+        self.assertEqual(resultado, (1, -10))
+
+    def test_polares(self):
+        # primer_test
+        resultado = new_complejos.polares([1, 1])
+        self.assertEqual(resultado, (1.41, 0.785))
+        # segundo_test
+        resultado = new_complejos.polares([3, 4])
+        self.assertEqual(resultado, (5, 0.927))
+        # tercer_test
+        resultado = new_complejos.polares([4, 3])
+        self.assertEqual(resultado, (5, 0.644))
+        # cuarto_test
+        resultado = new_complejos.polares([1, 0])
+        self.assertEqual(resultado, (1, 0))
+
+    def test_cartesianos(self):
+        # primer_test
+        resultado = new_complejos.cartesianos([1, 0])
+        self.assertEqual(resultado, (1, 0))
+        # segundo_test
+        resultado = new_complejos.cartesianos([0, 45])
+        self.assertEqual(resultado, (0, 0))
+        # tercer_test
+        resultado = new_complejos.cartesianos([2, 0.524])
+        self.assertEqual(resultado, (1.732, 1.001))
+        # cuarto_test
+        resultado = new_complejos.cartesianos([1.414, 0.785])
+        self.assertEqual(resultado, (1.0, 0.999))
+
+    def test_fase(self):
+        # primer_test
+        resultado = new_complejos.fase([1, 0])
+        self.assertEqual(resultado, 0)
+        # segundo_test
+        resultado = new_complejos.fase([1, -1])
+        self.assertEqual(resultado, 7.068)
+        # tercer_test
+        resultado = new_complejos.fase([-1.732, -1])
+        self.assertEqual(resultado, 3.666)
+        # cuarto_test
+        resultado = new_complejos.fase([-1, 1.732])
+        self.assertEqual(resultado, 4.189)
+
+    def test_suma_vecto(self):
+        # primer_test
+        resultado = new_complejos.suma_vecto([[1, 0], [2, 0]], [[1, 2], [3, 4]])
+        self.assertEqual(resultado, [(2, 2), (5, 4)])
+        # segundo_test
+        resultado = new_complejos.suma_vecto([[10, 0], [2, 0]], [[-10, 2], [-4, 4]])
+        self.assertEqual(resultado, [(0, 2), (-2, 4)])
+        # tercer_test
+        resultado = new_complejos.suma_vecto([[5 + 5, 0], [2, 5]], [[-15, 3], [3, -5]])
+        self.assertEqual(resultado, [(-5, 3), (5, 0)])
+        # cuarto_test
+        resultado = new_complejos.suma_vecto([[1, 0], [2, 0]], [[1, 2], [3, 4]])
+        self.assertEqual(resultado, [(2, 2), (5, 4)])
+
+    def test_inverse_vecto(self):
+        # primer_test
+        resultado = new_complejos.inverse_vecto([[1, 0], [2, 0]])
+        self.assertEqual(resultado, [(-1, 0), (-2, 0)])
+        # segunda_test
+        resultado = new_complejos.inverse_vecto([[1, -5], [2, -7]])
+        self.assertEqual(resultado, [(-1, 5), (-2, 7)])
+        # tercer_test
+        resultado = new_complejos.inverse_vecto([[1, 7], [2, -56]])
+        self.assertEqual(resultado, [(-1, -7), (-2, 56)])
+        # cuarto_test
+        resultado = new_complejos.inverse_vecto([[-4, 0], [2, 0]])
+        self.assertEqual(resultado, [(4, 0), (-2, 0)])
+
+    def test_multi_esc(self):
+        # primer_test
+        resultado = new_complejos.multi_esc((1, 0), [(2, 0), (5, 4)])
+        self.assertEqual(resultado, [(2, 0), (5, 4)])
+        # segundo_test
+        resultado = new_complejos.multi_esc((5, 1), [(3, 2), (5, 4)])
+        self.assertEqual(resultado, [(13, 13), (21, 25)])
+        # tercer_test
+        resultado = new_complejos.multi_esc((0, 0), [(3, 2), (5, 4)])
+        self.assertEqual(resultado, [(0, 0), (0, 0)])
+        # cuarto_test
+        resultado = new_complejos.multi_esc((3, 6), [(10, 7), (7, 3)])
+        self.assertEqual(resultado, [(-12, 81), (3, 51)])
+
+    def test_suma_mat(self):
+        # primer_test
+        resultado = new_complejos.suma_mat([[(1, 0), (2, 0)], [(3, 4), (4, 5)]], [[(4, 5), (1, 3)], [(6, 7), (8, 9)]])
+        self.assertEqual(resultado, [[(5, 5), (3, 3)], [(9, 11), (12, 14)]])
+        # segundo_test
+        resultado = new_complejos.suma_mat([[(1, 0), (2, 0)], [(3, 4), (4, 5)], [(1, 4), (3, 2)]],
+                                           [[(4, 5), (1, 3)], [(1, 4), (3, 2)], [(4, 4), (4, 5)]])
+        self.assertEqual(resultado, [[(5, 5), (3, 3)], [(4, 8), (7, 7)], [(5, 8), (7, 7)]])
+        # tercer_test
+        resultado = new_complejos.suma_mat([[(1, 1), (2, -3)], [(3, -4), (4, -5)]],
+                                           [[(-4, 5), (1, 3)], [(6, -7), (-8, 9)]])
+        self.assertEqual(resultado, [[(-3, 6), (3, 0)], [(9, -11), (-4, 4)]])
+        # cuarto_test
+        resultado = new_complejos.suma_mat([[(1, -5), (2, 5)], [(3, -4), (-4, 5)], [(1, -4), (3, -2)]],
+                                           [[(4, 5), (1, -3)], [(1, -4), (3, 2)], [(-4, 4), (4, 5)]])
+        self.assertEqual(resultado, [[(5, 0), (3, 2)], [(4, -8), (-1, 7)], [(-3, 0), (7, 3)]])
+
+    def test_inverse_mat(self):
+        # primer_test
+        resultado = new_complejos.inverse_mat([[(1, -5), (2, 5)], [(3, -4), (-4, 5)], [(1, -4), (3, -2)]])
+        self.assertEqual(resultado, [[(-1, 5), (-2, -5)], [(-3, 4), (4, -5)], [(-1, 4), (-3, 2)]])
+        # segundo_test
+        resultado = new_complejos.inverse_mat([[(1, 0), (2, 0)], [(3, 4), (4, 5)], [(1, 4), (3, 2)]])
+        self.assertEqual(resultado, [[(-1, 0), (-2, 0)], [(-3, -4), (-4, -5)], [(-1, -4), (-3, -2)]])
+        # tercer_test
+        resultado = new_complejos.inverse_mat([[(4, 5), (1, -3)], [(1, -4), (3, 2)], [(-4, 4), (4, 5)]])
+        self.assertEqual(resultado, [[(-4, -5), (-1, 3)], [(-1, 4), (-3, -2)], [(4, -4), (-4, -5)]])
+        # cuarto_test
+        resultado = new_complejos.inverse_mat([[(1, 1), (2, -3)], [(3, -4), (4, -5)]])
+        self.assertEqual(resultado, [[(-1, -1), (-2, 3)], [(-3, 4), (-4, 5)]])
+
+    def test_multi_esc_mat(self):
+        # primer_test
+        resultado = new_complejos.multi_esc_mat((1, 0), [[(1, 0), (2, 0)], [(3, 4), (4, 5)], [(1, 4), (3, 2)]])
+        self.assertEqual(resultado, [[(1, 0), (2, 0)], [(3, 4), (4, 5)], [(1, 4), (3, 2)]])
+        # segundo_test
+        resultado = new_complejos.multi_esc_mat((-4, 2), [[(4, 5), (1, 3)], [(1, 4), (3, 2)], [(4, 4), (4, 5)]])
+        self.assertEqual(resultado, [[(-26, -12), (-10, -10)], [(-12, -14), (-16, -2)], [(-24, -8), (-26, -12)]])
+        # tercer_test
+        resultado = new_complejos.multi_esc_mat((2, 3), [[(2, 0), (5, 4)], [(4, 5), (3, 5)]])
+        self.assertEqual(resultado, [[(4, 6), (-2, 23)], [(-7, 22), (-9, 19)]])
+        # cuarto_test
+        resultado = new_complejos.multi_esc_mat((-2, 10), [[(1, -5), (2, 5)], [(3, -4), (-4, 5)], [(1, -4), (3, -2)]])
+        self.assertEqual(resultado, [[(48, 20), (-54, 10)], [(34, 38), (-42, -50)], [(38, 18), (14, 34)]])
+
+    def test_traspuesta(self):
+        # primer_test
+        resultado = new_complejos.traspuesta([[(1, 0), (2, 0)]])
+        self.assertEqual(resultado, [[(1, 0)], [(2, 0)]])
+        # segundo_test
+        resultado = new_complejos.traspuesta([[(4, 5), (4, 3)]])
+        self.assertEqual(resultado, [[(4, 5)], [(4, 3)]])
+        # tercer_test
+        resultado = new_complejos.traspuesta([[(48, 20), (-54, 10)], [(34, 38), (-42, -50)], [(38, 18), (14, 34)]])
+        self.assertEqual(resultado, [[(48, 20), (34, 38), (38, 18)], [(-54, 10), (-42, -50), (14, 34)]])
+        # cuarto_test
+        resultado = new_complejos.traspuesta([[(1, -5), (2, 5)], [(3, -4), (-4, 5)], [(1, -4), (3, -2)]])
+        self.assertEqual(resultado, [[(1, -5), (3, -4), (1, -4)], [(2, 5), (-4, 5), (3, -2)]])
+
+    def test_conjugada_mat(self):
+        # primer_test
+        resultado = new_complejos.conjugada_mat([[(1, 0), (2, 0)], [(3, 4), (4, 3)]])
+        self.assertEqual(resultado, [[(1, 0), (2, 0)], [(3, -4), (4, -3)]])
+        # segundo_test
+        resultado = new_complejos.conjugada_mat([[(1, -5), (2, 5)], [(3, -4), (-4, 5)], [(1, -4), (3, -2)]])
+        self.assertEqual(resultado, [[(1, 5), (2, -5)], [(3, 4), (-4, -5)], [(1, 4), (3, 2)]])
+        # tercer_test
+        resultado = new_complejos.conjugada_mat([[(2, 0), (5, 4)], [(4, 5), (3, 5)]])
+        self.assertEqual(resultado, [[(2, 0), (5, -4)], [(4, -5), (3, -5)]])
+        # cuarto_test
+        resultado = new_complejos.conjugada_mat([[(48, 20), (-54, 10)], [(34, 38), (-42, -50)], [(38, 18), (14, 34)]])
+        self.assertEqual(resultado, [[(48, -20), (-54, -10)], [(34, -38), (-42, 50)], [(38, -18), (14, -34)]])
+
+    def test_adjunta(self):
+        # primer_test
+        resultado = new_complejos.adjunta([[(48, 20), (-54, 10)], [(34, 38), (-42, -50)], [(38, 18), (14, 34)]])
+        self.assertEqual(resultado, [[(48, -20), (34, -38), (38, -18)], [(-54, -10), (-42, 50), (14, -34)]])
+        # segundo_test
+        resultado = new_complejos.adjunta([[(1, -5), (2, 5)], [(3, -4), (-4, 5)], [(1, -4), (3, -2)]])
+        self.assertEqual(resultado, [[(1, 5), (3, 4), (1, 4)], [(2, -5), (-4, -5), (3, 2)]])
+        # tercer_test
+        resultado = new_complejos.adjunta([[(1, 0), (2, 0)]])
+        self.assertEqual(resultado, [[(1, 0)], [(2, 0)]])
+        # cuarto_test
+        resultado = new_complejos.adjunta([[(4, 5), (4, 3)]])
+        self.assertEqual(resultado, [[(4, -5)], [(4, -3)]])
+
+    def test_producto(self):
+        # primer_test
+        resultado = new_complejos.producto([[(1, 0), (2, 0)], [(3, 4), (4, 5)]], [[(4, 5), (1, 3)], [(6, 7), (8, 9)]])
+        self.assertEqual(resultado, [[(16, 19), (17, 21)], [(-19, 89), (-22, 89)]])
+        # segundo_test
+        resultado = new_complejos.producto([[(1, 0), (2, 0), (3, 5)], [(3, 4), (4, 5), (6, 9)]],
+                                           [[(4, 5), (1, 3)], [(6, 7), (8, 9)]])
+        self.assertEqual(resultado, "Is not possible")
+        # tercer_test
+        resultado = new_complejos.producto([[(1, 0), (0, 0)], [(0, 0), (1, 0)]], [[(4, 5), (1, 3)], [(6, 7), (8, 9)]])
+        self.assertEqual(resultado, [[(4, 5), (1, 3)], [(6, 7), (8, 9)]])
+        # cuarto_test
+        resultado = new_complejos.producto([[(1, 0), (0, 0)], [(0, 0), (1, 0)]], [[(6, 7), (8, 9)]])
+        self.assertEqual(resultado, "Is not possible")
+
+    def test_accion(self):
+        # primer_test
+        resultado = new_complejos.Accion([[(1, 0), (2, 0)], [(3, 4), (4, 5)]], [[(4, 5)], [(1, 3)]])
+        self.assertEqual(resultado, [[(6, 11)], [(-19, 48)]])
+        # segundo_test
+        resultado = new_complejos.Accion([[(1, 0), (2, 0)], [(3, 4), (4, 5)]], [[(4, 5), (1, 3)]])
+        self.assertEqual(resultado, "Is not possible")
+        # tercer_test
+        resultado = new_complejos.Accion([[(1, 0), (2, 0), (6, 45)], [(3, 4), (4, 5), (4, 6)], ],
+                                         [[(4, 5)], [(1, 3)], [(29, 54)]])
+        self.assertEqual(resultado, [[(-2250, 1640)], [(-227, 438)]])
+        # cuarto_test
+        resultado = new_complejos.Accion([[(1, 0), (2, 0), (6, 45), (4, 6)], [(3, 4), (4, 5), (4, 6), (3, 7)], ],
+                                         [[(4, 5)], [(1, 3)], [(29, 54)], [(31, 66)]])
+        self.assertEqual(resultado, [[(-2522, 2090)], [(-596, 853)]])
+
+    def test_producto_interno(self):
+        # primer_test
+        resultado = new_complejos.product_int([(3, 1), (2, 6), (7, 5), (3, 6)], [(4, 5), (1, 3), (29, 54), (31, 66)])
+        self.assertEqual(resultado, (-379, 938))
+        # segundo_test
+        resultado = new_complejos.product_int([(1, 0), (1, 0), (0, 1), (0, 1)], [(4, 5), (1, 3), (29, 54), (31, 66)])
+        self.assertEqual(resultado, (-115, 68))
+        # tercer_test
+        resultado = new_complejos.product_int([(0, 0), (0, 0), (0, 0), (0, 0)], [(4, 5), (1, 3), (29, 54), (31, 66)])
+        self.assertEqual(resultado, (0, 0))
+        # cuarto_test
+        resultado = new_complejos.product_int([(4, 2), (2, 6), (6, 3), (7, 8)], [(5, 1), (4, 2), (6, 3), (1, 7)])
+        self.assertEqual(resultado, (-8, 135))
+
+    def test_norma(self):
+        # primer_test
+        resultado = new_complejos.norma([(4, 2), (2, 6), (6, 3), (7, 8)])
+        self.assertEqual(resultado, 14.76)
+        # segundo_test
+        resultado = new_complejos.norma([(5, 1), (4, 2), (6, 3), (1, 7)])
+        self.assertEqual(resultado, 11.87)
+        # tercer_test
+        resultado = new_complejos.norma([(4, 5), (1, 3), (29, 54), (31, 66)])
+        self.assertEqual(resultado, 95.52)
+        # cuarto_test
+        resultado = new_complejos.norma([[1, 0], [2, 0]])
+        self.assertEqual(resultado, 2.24)
+
+    def test_distancia(self):
+        # primer_test
+        resultado = new_complejos.distancia([(5, 1), (4, 2), (6, 3), (1, 7)], [(3, 1), (2, 6), (7, 5), (3, 6)])
+        self.assertEqual(resultado, 5.83)
+        # segundo_test
+        resultado = new_complejos.distancia([(1, 0), (2, 0)], [(3, 4), (4, 5)])
+        self.assertEqual(resultado, 7.0)
+        # tercer_test
+        resultado = new_complejos.distancia([(4, 5), (1, 3)], [(3, 4), (4, 5)])
+        self.assertEqual(resultado, 3.88)
+        # cuarto_test
+        resultado = new_complejos.distancia([(7, 3), (3, 5), (7, 4), (8, 5)], [(1, 2), (7, 3), (7, 8), (2, 4)])
+        self.assertEqual(resultado, 10.48)
+
+    def test_unit(self):
+        # primer_test
+        resultado = new_complejos.unit([[(2 / 3, 0.0), (-2 / 3, 1 / 3)], [(2 / 3, 1 / 3), (2 / 3, 0.0)]])
+        self.assertEqual(resultado, True)
+        # primer_test
+        resultado = new_complejos.unit([[(0, 0), (0, 1)], [(0, 1), (0, 0)]])
+        self.assertEqual(resultado, True)
+        # primer_test
+        resultado = new_complejos.unit([[(0, 2), (0, 1)], [(0, 4), (2, 0)]])
+        self.assertEqual(resultado, False)
+        # primer_test
+        resultado = new_complejos.unit([[(1, 0), (2, 0)], [(0, 4), (2, 0)], [(0, 4), (2, 0)]])
+        self.assertEqual(resultado, "The input have to be a square matrix")
+
+    def test_herm(self):
+        # primer_test
+        resultado = new_complejos.herm([[(7, 0), (6, 5)], [(6, -5), (-3, 0)]])
+        self.assertEqual(resultado, True)
+        # primer_test
+        resultado = new_complejos.herm([[(5, 1), (4, 2)], [(6, 3), (1, 7)]])
+        self.assertEqual(resultado, False)
+        # primer_test
+        resultado = new_complejos.herm([[(5, 0), (3, 7)], [(3, -7), (2, 0)]])
+        self.assertEqual(resultado, True)
+        # primer_test
+        resultado = new_complejos.herm([[(1, 0), (2, 4)], [(2, -4), (3, 0)]])
+        self.assertEqual(resultado, True)
+
+
+if __name__ == '__main__':
+    unittest.main()
---
+++ new_complejos.py
@@ -0,0 +1,251 @@
+import math
+
+
+def suma(num1, num2):
+    suma1 = round(num1[0] + num2[0], 3)
+    suma2 = round(num1[1] + num2[1], 3)
+    resultado = suma1, suma2
+    return resultado
+
+
+def resta(num1, num2):
+    resta1 = round(num1[0] - num2[0], 3)
+    resta2 = round(num1[1] - num2[1], 3)
+    resultado = resta1, resta2
+    return resultado
+
+
+def multi(num1, num2):
+    rest1 = round((num1[0] * num2[0]) - (num1[1] * num2[1]), 3)
+    rest2 = round((num1[0] * num2[1]) + (num1[1] * num2[0]), 3)
+    resultado = rest1, rest2
+    return resultado
+
+
+def division(num1, num2):
+    div1 = round(((num1[0] * num2[0]) + (num1[1] * num2[1])) / ((num2[0] ** 2) + (num2[1] ** 2)), 3)
+    div2 = round(((num1[1] * num2[0]) - (num1[0] * num2[1])) / ((num2[0] ** 2) + (num2[1] ** 2)), 3)
+    resultado = div1, div2
+    return resultado
+
+
+def modulo(num1):
+    rta = round(((num1[0] ** 2) + (num1[1] ** 2)) ** (1 / 2), 2)
+    return rta
+
+
+def conjugado(num1):
+    img = num1[1] * -1
+    rta = num1[0], img
+    return rta
+
+
+def polares(num1):
+    p = modulo(num1)
+    theta = round(math.atan(num1[1] / num1[0]), 3)
+    resultado = p, theta
+    return resultado
+
+
+def cartesianos(num1):
+    a = round(num1[0] * (math.cos(num1[1])), 3)
+    b = round(num1[0] * (math.sin(num1[1])), 3)
+    resultado = a, b
+    return resultado
+
+
+def fase(num1):
+    fase1 = round(math.atan(num1[1] / num1[0]), 3)
+    if num1[0] < 0 and num1[1] > 0:
+        fase1 = round(math.pi - fase1, 3)
+    elif num1[0] < 0 and num1[1] < 0:
+        fase1 = round(math.pi + fase1, 3)
+    elif num1[0] > 0 and num1[1] < 0:
+        fase1 = round(2 * math.pi - fase1, 3)
+    return fase1
+
+
+def suma_vecto(vect1, vect2):
+    resultado = []
+    if len(vect1) != len(vect1):
+        print("Error: diferent lenght vectors")
+    for i in range(len(vect1)):
+        suma_real = suma(vect1[i], vect2[i])
+        resultado += [suma_real]
+    return resultado
+
+
+def inverse(num):
+    num2 = num[0] * -1, num[1] * -1
+    return num2
+
+
+def inverse_vecto(vect):
+    for i in range(len(vect)):
+        vect[i] = inverse(vect[i])
+    return vect
+
+
+def multi_esc(num, vect):
+    resultado = []
+    for i in range(len(vect)):
+        multi_vect = multi(num, vect[i])
+        resultado += [multi_vect]
+    return resultado
+
+
+def suma_mat(mat1, mat2):
+    resultado = []
+    for i in range(len(mat1)):
+        suma = suma_vecto(mat1[i], mat2[i])
+        resultado += [suma]
+    return resultado
+
+
+def inverse_mat(mat):
+    for i in range(len(mat)):
+        for j in range(len(mat[0])):
+            mat[i][j] = inverse(mat[i][j])
+    return mat
+
+
+def multi_esc_mat(num, mat):
+    resultado = []
+    for i in range(len(mat)):
+        mult = multi_esc(num, mat[i])
+        resultado += [mult]
+    return resultado
+
+
+def traspuesta(vect):
+    n = len(vect)  # filas
+    m = len(vect[0])  # columnas
+    matriz = [[0 for i in range(n)] for j in range(m)]
+    for i in range(n):
+        for j in range(m):
+            matriz[j][i] = vect[i][j]
+    return matriz
+
+
+def conjugada_mat(mat):
+    for i in range(len(mat)):
+        for j in range(len(mat[0])):
+            mat[i][j] = conjugado(mat[i][j])
+    return mat
+
+
+def adjunta(mat):
+    mat2 = [[mat[i][j] for j in range(len(mat[0]))] for i in range(len(mat))]
+    mat2 = conjugada_mat(mat2)
+    mat2 = traspuesta(mat2)
+    return mat2
+
+
+def producto(mat1, mat2):
+    if len(mat1[0]) != len(mat2):
+        return "Is not possible"
+    else:
+        matriz = [[(0, 0) for i in range(len(mat2[0]))] for j in range(len(mat1))]
+        for i in range(len(mat1)):
+            for j in range(len(mat2[0])):
+                for k in range(len(mat1[0])):
+                    matriz[i][j] = suma(matriz[i][j], multi(mat1[i][k], mat2[k][j]))
+    return matriz
+
+
+def Accion(mat, vect):
+    rta = producto(mat, vect)
+    return rta
+
+
+def product_int(vect1, vect2):
+    if isinstance(vect1[0], tuple) or isinstance(vect2[0], tuple):
+        rta = (0, 0)
+        if len(vect1) == len(vect2):
+            for i in range(len(vect1)):
+                rta = suma(rta, (multi(vect1[i], vect2[i])))
+        return rta
+    else:
+        vect = adjunta(vect1)
+        rta = producto(vect, vect2)
+        return rta[0][0]
+
+
+def norma(vect):
+    suma = 0
+    for i in range(len(vect)):
+        suma += (modulo(vect[i]) ** 2)
+    return round(suma ** (1 / 2), 2)
+
+
+def distancia(vect1, vect2):
+    resultado = []
+    if len(vect1) != len(vect1):
+        print("Error: diferent lenght vectors")
+    else:
+        for i in range(len(vect1)):
+            resta_vect = resta(vect1[i], vect2[i])
+            resultado += [resta_vect]
+    return abs(norma(resultado))
+
+
+def unit(mat):
+    if len(mat)== len(mat[0]):
+        In = [[(0, 0) for j in range(len(mat[0]))] for i in range(len(mat))]
+        for i in range(len(mat)):
+            for j in range(len(mat[0])):
+                if i == j:
+                    In[i][j] = (1, 0)
+        mat1 = adjunta(mat)
+        product = producto(mat1, mat)
+        rta = True
+        for i in range(len(mat)):
+            for j in range(len(mat)):
+                if product[i][j] != In[i][j]:
+                    rta = False
+        if rta:
+            return  True
+        else:
+            return  False
+    else:
+        return "The input have to be a square matrix"
+
+
+def herm(mat):
+    aux = mat[:]
+    mat2 = adjunta(aux)
+    rta = True
+    for i in range(len(mat)):
+        for j in range(len(mat[0])):
+            if mat[i][j] == mat2[i][j] and mat[i][j] == mat2[i][j]:
+                continue
+            else:
+                rta = False
+    return rta
+# print(producto([[[3, 2], [6, 3], [0, 0]], [[5, 1], [4, 0], [2, 3]], [[4, 3], [5, 1], [4, 2]]],[[[1, 0], [2, 3], [3, 4]], [[1, 4], [5, 6], [6, 4]], [[4, 3], [5, 1], [4, 2]]]))
+# print(multi_esc_mat([3, 2], [[[1, 0], [2, 3], [3, 4]], [[1, 4], [5, 6], [6, 4]]]))
+# print(adjunta([[[1, 0], [2, 3], [3, 4]], [[1, 4], [5, 6], [6, 4]]]))
+# print(unit([[(1/2, 1/2), (0,1/(math.sqrt(3))), (3/2*(math.sqrt(15)),1/2*(math.sqrt(15)))],[(-1/2,0),(1/(math.sqrt(3)),0),(4/2*(math.sqrt(15)),3/2*(math.sqrt(15)))],[(1/2,0),(0,-1/(math.sqrt(3))),(0,5/2*(math.sqrt(15)))]]))
+# print(distancia([[3, 0], [1, 0], [2, 0]],[[2,0],[2,0],[-1,0]]))print(unit([[(0, 0), (0, 1)], [(0, 1), (0, 0)]]))
+# print(producto([[[2/3, 0], [-2/3, 1/3]], [[2/3, 1/3], [2/3, 0]]],[[[0.6666666666666666, 0], [0.6666666666666666, -0.3333333333333333]], [[-0.6666666666666666, -0.3333333333333333], [0.6666666666666666, 0]]]))
+# print(unit([[[2/3, 0], [-2/3, 1/3]], [[2/3, 1/3], [2/3, 0]]]))
+# print(adjunta([[[2/3, 0], [-2/3, 1/3]], [[2/3, 1/3], [2/3, 0]]]))
+# mat = [[[7, 0], [6, 5]], [[6, -5], [-3, 0]]]
+# for row in mat:
+#    print(row)
+# mat2 = transpuesta([[[7, 0], [6, 5]], [[6, -5], [-3, 0]]])
+# for row in mat2:
+#   print(row)
+# mat3 = conjugada_mat(mat2)
+# for row in mat3:
+#    print(row)
+# mat4 = adjunta(mat)
+# for row in mat4:
+#   print(row)
+# print(distancia([(4, 5), (1, 3)],[(3, 4), (4, 5)]))
+print(herm([[(7, 0), (6, 5)], [(6, -5), (-3, 0)]]))
+# [[1, 0], [2, 3], [3, 4]]
+# [[1, 4], [5, 6], [6, 4]]
+# [[1, 0], 0]
+# [[2, 3], 0]
+# [[3, 4], 0]
